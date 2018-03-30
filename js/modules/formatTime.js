a4d2ec069860e7ba3e19667cd906c5844f86b67d
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-30 20:47:02 +0200

    require colors package and change console.log color

--- js/modules/formatTime.js
+++ js/modules/formatTime.js
@@ -1,13 +1,13 @@
 const os = require('os');
+const colors = require('colors');
 
 function calcTime(time) {
-  time = os.uptime();
   let hours = Math.floor(time / 3600);
   time = time - hours * 3600;
   let minutes = Math.floor(time / 60);
   let seconds = Math.floor(time - minutes * 60);
   time = hours + 'h ' + minutes + 'min ' + seconds + 'sec';
-  console.log('Uptime is: ' + time);
+  console.log(colors.rainbow('Uptime is: ') + time);
 }
 
 exports.print = calcTime;
